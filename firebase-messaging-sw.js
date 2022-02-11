effd6ad57d814a8886daed5a725e99d950667dc7
fahim27 <fahimmbpi@gmail.com>
2022-02-11 21:10:15 +0600

    first commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
+/*
+Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+*/
+firebase.initializeApp({
+    apiKey: "AIzaSyAKI9RkhRvBUtBVjMpHiPJdjLV7tz6j_AA",
+    authDomain: "push-notifications-91568.firebaseapp.com",
+    projectId: "push-notifications-91568",
+    storageBucket: "push-notifications-91568.appspot.com",
+    messagingSenderId: "216410021398",
+    appId: "1:216410021398:web:badfe2fdca5dbdbb911f37"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log("Message received.", payload);
+    const title = "Hello world is awesome";
+    const options = {
+        body: "Your notificaiton message .",
+        icon: "/firebase-logo.png",
+    };
+    return self.registration.showNotification(
+        title,
+        options,
+    );
+});
